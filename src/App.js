dd4d941d31c9b2458ba1ca0111183c3970f9565d
kimia <kimiahaghighi9877@gmail.com>
2022-03-18 19:19:48 +0330

    build search engine

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import Dictionary from './Dictionary' ;
 
 function App() {
+
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
       </header>
+      <main>
+        <Dictionary/>
+      </main>
+
+      <footer className='text-center'>
+        Coded by Kimia Haghighi 🦕
+      </footer>
+
+     </div>
     </div>
   );
 }
