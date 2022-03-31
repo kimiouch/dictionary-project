5ff75e5d4765a52494f25ffbb22c6405f5985673
kimia <kimiahaghighi9877@gmail.com>
2022-03-31 21:05:36 +0430

    added dictionary API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,13 +1,20 @@
+import axios from "axios";
 import React, {useState} from "react";
 import "./Dictionary.css"
 
 export default function Dictionary(){
     let [word, setWord] = useState("");
 
+    function handleresponse(response){
+        console.log(response.data[0]);
+    }
+
 
     function search(event) {
         event.preventDefault();
-        alert(`Searching for ${word}`);
+
+        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+        axios.get(apiUrl).then(handleresponse);
     }
     
     function handleWordChange(event) {
