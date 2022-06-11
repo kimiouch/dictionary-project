fcaa6485cf793fd41f585681258f5b4e17c40b7f
kimia <kimiahaghighi9877@gmail.com>
2022-06-11 16:42:56 +0430

    add phonetic component, debug meaning/synonyms/results

--- src/Meaning.js
+++ src/Meaning.js
@@ -4,23 +4,20 @@ import Synonyms from "./Synonyms"
 export default function Meaning(props){
   return (
     <div className="Meaning">
-      <h3> {props.meaning.partOfSpeech} </h3>
-      
 
-      {props.meaning.definitions.map(function (definition, index)
-        {
+      <h3> {props.meaning.partOfSpeech} </h3>
+      {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
-            <p>
+               <strong> Definition:</strong>
                {definition.definition}
-             <br/>
-               {definition.example}
-              <br/>
-              <Synonyms synonyms={definition.synonyms}/> 
-            </p>
+                  <br/>
+               <strong> {definition.example} </strong>
+                  <br/>
+              <Synonyms synonyms={definition.synonyms} /> 
          </div>
        );
-        })}
+      })}
     </div>
-  )
+  );
 }
--- src/Results.js
+++ src/Results.js
@@ -1,19 +1,28 @@
 import React from "react";
 import Meaning from "./Meaning";
+import Phonetic from "./Phonetic"
 
 export default function Results(props) {
   if (props.result) {
     return (
     <div className="Results"> 
       <h2>{props.result.word}</h2>
+
+      {props.result.phonetics.map(function(phonetic, index) {
+        return (
+          <div key={index}>
+            <Phonetic phonetic={phonetic} />
+          </div>
+        )
+      })}
+
       {props.result.meanings.map(function(meaning, index) {
         return (
           <div key={index}>
             <Meaning meaning={meaning} />
           </div>
         )
-      }
-      )}
+      })}
     </div>
     )
   } else {
